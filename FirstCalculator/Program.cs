1c48e08ea303b13412922480c953776124e29434
s198061 <s198061@LAF-7E868C>
2019-08-21 17:54:06 -0500

    Add project files.

---
+++ FirstCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+            Console.WriteLine(num);
+            Console.ReadKey();
+            num += num;
+            Console.ReadKey();
+            Console.WriteLine(num);
+            Console.ReadKey();
+            num -= num;
+            Console.ReadKey();
+            Console.WriteLine(num);
+            Console.ReadKey();
+            num *= num;
+            Console.ReadKey();
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+
+    }
+}
